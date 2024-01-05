f369e2383c6c886c1a81e5ea1b850c4a5f364991
d1onys1us <13951458+d1onys1us@users.noreply.github.com>
2024-01-04 22:22:37 -0500

    fixup

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://docs.taiko.xyz",
   redirects: {
     "/": "/start-here/getting-started",
   },
